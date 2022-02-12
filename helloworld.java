4f52fae415c9dff21702141941eefcccd620dcd3
selome zewdie <98489447+se-sudo@users.noreply.github.com>
2022-02-12 08:39:18 -0600

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello world : triggered by webhook!");
+    System.out.println("Hello world : triggered by webhook1!");
     }
 }
